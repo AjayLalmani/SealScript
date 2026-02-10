581f42459c420128507047c254958b1f9be5812a
Ajay Lalmani <ajaylalmani716@gmail.com>
2026-02-10 23:56:26 +0530

    First Commit

---
+++ Backend/server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const dbUrl = process.env.MONGO_URL;
+mongoose
+  .connect(dbUrl)
+  .then(() => console.log("Db Connected Successfully"))
+  .catch((e) => console.log("DB connect Error : ", e));
+
+app.use(cors());
+app.use(express.json());
+
+const Port = process.env.PORT || 5000;
+
+app.use("/api/auth", require("./routes/auth"));
+
+
+app.listen(Port, () => {
+  console.log(`listening at ${Port}`);
+});
